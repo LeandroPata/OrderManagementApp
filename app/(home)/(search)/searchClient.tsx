04c9949475f57a0c92e41c6fc958ba2686dc557d
LeandroPata <lmfpata@gmail.com>
2024-12-21 16:17:13 +0000

    Placeholder search screens

---
+++ app/(home)/(search)/searchClient.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const SearchClient = () => {
+  return (
+    <View>
+      <Text>SearchClient</Text>
+    </View>
+  );
+};
+
+export default SearchClient;
---
+++ app/(home)/(search)/searchProduct.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const SearchProduct = () => {
+  return (
+    <View>
+      <Text>SearchProduct</Text>
+    </View>
+  );
+};
+
+export default SearchProduct;
