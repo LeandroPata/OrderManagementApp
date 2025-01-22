7a3f23c0d168f132323aec48c40bf44d46478162
LeandroPata <lmfpata@gmail.com>
2025-01-22 17:29:57 +0000

    Updated version number, changed splash screen and app icon

--- app.config.ts
+++ app.config.ts
@@ -4,9 +4,9 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
 	...config,
 	name: 'OrderManagementApp',
 	slug: 'OrderManagementApp',
-	version: '1.0.0',
+	version: '0.10.1',
 	orientation: 'portrait',
-	icon: './assets/images/icon.png',
+	icon: './assets/images/logoReact.png',
 	scheme: 'myapp',
 	userInterfaceStyle: 'automatic',
 	newArchEnabled: false,
@@ -18,13 +18,26 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
 	},
 	android: {
 		adaptiveIcon: {
-			foregroundImage: './assets/images/adaptive-icon.png',
-			backgroundColor: '#ffffff',
+			foregroundImage: './assets/images/logoReact.png',
+			backgroundColor: '#fffbff',
 		},
 		package: 'com.leandropata.ordermanagementapp',
 		permissions: [
 			'android.permission.READ_EXTERNAL_STORAGE',
 			'android.permission.WRITE_EXTERNAL_STORAGE',
+			'android.permission.REQUEST_INSTALL_PACKAGES',
+		],
+		intentFilters: [
+			{
+				action: 'VIEW',
+				data: [
+					{
+						scheme: 'file',
+						mimeType: 'application/vnd.android.package-archive',
+					},
+				],
+				category: ['BROWSABLE', 'DEFAULT'],
+			},
 		],
 		googleServicesFile:
 			process.env.GOOGLE_SERVICES_JSON || './google-services.json',
@@ -32,20 +45,37 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
 	web: {
 		bundler: 'metro',
 		output: 'static',
-		favicon: './assets/images/favicon.png',
+		favicon: './assets/images/logoReact.png',
 	},
 	plugins: [
 		'expo-router',
 		[
 			'expo-splash-screen',
 			{
-				image: './assets/images/splash-icon.png',
+				image: './assets/images/logoReact.png',
 				imageWidth: 200,
 				resizeMode: 'contain',
-				backgroundColor: '#ffffff',
+				backgroundColor: '#fffbff',
+				dark: {
+					image: './assets/images/logoReact.png',
+					imageWidth: 200,
+					resizeMode: 'contain',
+					backgroundColor: '#191918',
+				},
 			},
 		],
-		['expo-build-properties', { ios: { useFrameworks: 'static' } }],
+		[
+			'expo-build-properties',
+			{
+				android: {
+					enableProguardInReleaseBuilds: true,
+					enableShrinkResourcesInReleaseBuilds: true,
+				},
+				ios: {
+					useFrameworks: 'static',
+				},
+			},
+		],
 		'expo-localization',
 		'@react-native-firebase/app',
 		'@react-native-firebase/auth',
